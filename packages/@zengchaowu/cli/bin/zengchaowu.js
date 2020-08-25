b3d6b573f566e27f846d1ac698409f97f7964f44
zengchaowu <admin@zengchaowu.com>
2020-08-25 13:48:49 +0800

    mmm

--- packages/@zengchaowu/cli/bin/zengchaowu.js
+++ packages/@zengchaowu/cli/bin/zengchaowu.js
@@ -12,21 +12,8 @@ program
     .usage('<command> [options]')
 
 program
-    .command('create <app-name>')
-    .description('create a new project powered by vue-cli-service')
-    .option('-p, --preset <presetName>', 'Skip prompts and use saved or remote preset')
-    .option('-d, --default', 'Skip prompts and use default preset')
-    .option('-i, --inlinePreset <json>', 'Skip prompts and use inline JSON string as preset')
-    .option('-m, --packageManager <command>', 'Use specified npm client when installing dependencies')
-    .option('-r, --registry <url>', 'Use specified npm registry when installing dependencies (only for npm)')
-    .option('-g, --git [message]', 'Force git initialization with initial commit message')
-    .option('-n, --no-git', 'Skip git initialization')
-    .option('-f, --force', 'Overwrite target directory if it exists')
-    .option('--merge', 'Merge target directory if it exists')
-    .option('-c, --clone', 'Use git clone when fetching remote preset')
-    .option('-x, --proxy <proxyUrl>', 'Use specified proxy when creating project')
-    .option('-b, --bare', 'Scaffold project without beginner instructions')
-    .option('--skipGetStarted', 'Skip displaying "Get started" instructions')
+    .command('create <project-name>')
+    .option('-p, --platform', 'Target platform')
     .action((name, cmd) => {
         const options = cleanArgs(cmd)
 
