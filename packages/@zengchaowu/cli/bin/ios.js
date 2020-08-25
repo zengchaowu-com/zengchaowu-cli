cac173fdebc33c080ccfc87bc134803086dd0efb
zengchaowu <admin@zengchaowu.com>
2020-08-25 15:59:38 +0800

    mmm

--- packages/@zengchaowu/cli/bin/ios.js
+++ packages/@zengchaowu/cli/bin/ios.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
 
+const { inquirer } = require('@zengchaowu/cli-shared-utils')
+
+// 选择工作流
+inquirer.prompt([
+    {
+        type: 'input', // 问题类型
+        name: 'name', // 数据属性名
+        message: '名称', // 提示信息
+        default: 'Rogan' // 默认值
+    }
+]).then(answers => {
+    // 处理结果
+    console.log(`你的名字: `, answers.name)
+
+})
