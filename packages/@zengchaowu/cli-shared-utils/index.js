68e821a2d41310000dbc965499be6f739a4f06e4
zengchaowu <admin@zengchaowu.com>
2020-08-25 12:45:17 +0800

    mmm

---
+++ packages/@zengchaowu/cli-shared-utils/index.js
@@ -0,0 +1,2 @@
+// 命令行颜色工具
+exports.chalk = require('chalk')
--- packages/@zengchaowu/cli/bin/zengchaowu.js
+++ packages/@zengchaowu/cli/bin/zengchaowu.js
@@ -3,8 +3,7 @@
 // Check node version before requiring/doing anything else
 // The user may be on a very old node version
 
-const { chalk, semver } = require('@zengchaowu/cli-shared-utils')
+const { chalk, leven } = require('@zengchaowu/cli-shared-utils')
 const requiredVersion = require('../package.json').engines.node
-const leven = require('leven')
 
-console.log(requiredVersion)
+console.log(chalk.red(requiredVersion))
