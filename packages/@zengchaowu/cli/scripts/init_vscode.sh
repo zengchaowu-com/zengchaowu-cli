1bad09eed2ab27029995e8b1c14c7ceec50b775a
chaowu zeng <chaowuzeng@MacBook-Pro.lan>
2020-09-02 18:09:00 +0800

    mmm

--- packages/@zengchaowu/cli/scripts/init_vscode.sh
+++ packages/@zengchaowu/cli/scripts/init_vscode.sh
@@ -4,6 +4,9 @@
 
 ## Python
 @id:ms-python.python
-
+提示安装pylint
 ## 使用CommandLineTools中Python3.7作为默认python环境
 echo 'export PATH="/Users/chaowuzeng/Library/Python/3.7/bin"' >> .zshrc
+
+## Ruby
+@id:rebornix.ruby
